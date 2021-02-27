f1c4026b8be35e46723d1ac70df8b9b6ea262a9d
13Rei <reignition13@protonmail.com>
2021-02-27 10:56:33 +0100

    comments functionality update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,14 +5,14 @@ class CommentsController < ApplicationController
         @comment = @post.comments.create(params[:comment].permit(:name, :body))
         redirect_to post_path(@post)
     end
-
-    def update
-        update_attribute(:isCorrect => 1)
-    end
-
+    
     def destroy
         @post = Post.find(params[:post_id])
         @post.comments.find(params[:id]).destroy
         redirect_to post_path(@post)
+    end
+    
+    def update
+        update_attribute(:isCorrect => 1)
     end
 end
