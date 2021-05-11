c1e75a88e165bf96313361e22b03ada698866197
13Rei <reignition13@protonmail.com>
2021-05-11 13:14:44 +0200

    old

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable,
-         :omniauthable, :omniauth_providers => [:google_oauth2]
+         :omniauthable, omniauth_providers: %i[google_oauth2] 
 
 
   #def self.from_omniauth(access_token)
