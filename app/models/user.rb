1610f9b462851db81a42ec8969daecd42c4bf149
13Rei <59819727+13Rei@users.noreply.github.com>
2021-05-12 09:17:53 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,24 +6,9 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[facebook] 
 
 
-  #def self.from_omniauth(access_token)
-  #    data = access_token.info
-  #    user = User.where(email: data['email']).first
-  
-  #    # Uncomment the section below if you want users to be created if they don't exist
-  #    unless user
-  #        user = User.create(name: data['name'],
-  #           email: data['email'],
-  #           password: Devise.friendly_token[0,20]
-  #        )
-  #    end
-  #    user
-  #end
-
   def self.from_omniauth(auth)
-    name_split = auth.info.name.split(" ")
     user = User.where(email: auth.info.email).first
-    user ||= User.create!(provider: auth.provider, uid: auth.uid, name: auth.info.name, email: auth.info.email, encrypted_password: Devise.friendly_token[0, 20])
+    user ||= User.create!(provider: auth.provider, uid: auth.uid, name: auth.info.name, email: auth.info.email, password: Devise.friendly_token[0,20], encrypted_password: Devise.friendly_token[0, 20])
       user
   end
 
