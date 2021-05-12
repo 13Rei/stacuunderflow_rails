a96502d0c113adee973ae2d271de81ed90805041
13Rei <59819727+13Rei@users.noreply.github.com>
2021-05-12 09:17:03 +0200

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,45 +2,20 @@
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  def google_oauth2
-    p request.env, '---'
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+  
+  def facebook
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
     if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
     else
-        flash[:error] = 'There was a problem singing you in through Google. Please register or try signing in later.'
-        redirect_to new_user_registration_url
+      session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
+      redirect_to new_user_registration_url
     end
   end
 
   def failure
-    flash[:error] = 'There was a problem signing you in. Please register or try signing in later.'
-    redirect_to new_user_registration_url
+    redirect_to root_path
   end
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
-
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-
-  # More info at:
-  # https://github.com/heartcombo/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  def passthru
-    google_oauth2()
-  end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
 end
